97c3fe5a240ef07927122b599bccbc329bfcfab3
mikepaiv@gmail.com <mikepaiv@gmail.com>
2020-01-29 21:03:55 -0300

    Classe Categoria

---
+++ src/main/java/com/mikepaiva/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.mikepaiva.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class CategoriaResource {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		return "Rest está funcionando";
+
+	}
+}
