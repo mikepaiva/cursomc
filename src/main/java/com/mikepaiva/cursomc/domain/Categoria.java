6214a9dc18bc3c19974d9fec7860f9ab32ebda1f
mikepaiv@gmail.com <mikepaiv@gmail.com>
2020-01-29 23:10:52 -0300

    Testando a primeira classe de domínio

---
+++ src/main/java/com/mikepaiva/cursomc/domain/Categoria.java
@@ -0,0 +1,35 @@
+package com.mikepaiva.cursomc.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+
+	public Categoria() {
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
--- src/main/java/com/mikepaiva/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/mikepaiva/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.mikepaiva.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mikepaiva.cursomc.domain.Categoria;
+
 @RestController
-@RequestMapping
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+		
+		return lista;
 
 	}
 }
